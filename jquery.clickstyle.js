6f357a3bb2e55500df0d3b8848b73892a0da705d
jwbmedia <jacob@jwbmedia.com>
2015-03-11 21:28:24 -0600

    Revert "Revert "Default not set to click""
    This reverts commit 52e034f2beecfa0ad9ab69f9078032cfad10014c.

--- jquery.clickstyle.js
+++ jquery.clickstyle.js
@@ -4,27 +4,44 @@
             color: "white",
             background: "#29bf35",
             text: "Success!",
-            rotate: "yes",
+            rotate: true,
             transition: "1000",
-            bordercolor: "none"
+            bordercolor: "none",
+            onclick: true
         }, options);
 
         return this.each(function () {
-            $(this).click(function () {
+            if (settings.onclick == true) {
+                $(this).click(function () {
+                    $(this).text(settings.text);
+                    $(this).css("color", settings.color);
+                    $(this).css("background", settings.background);
+                    if (settings.bordercolor == "none") {
+                        $(this).css("border", "none");
+                    } else {
+                        $(this).css("border", "1px solid" + settings.border);
+                    }
+                    $(this).css("transition", settings.transition + "ms" + " " + "ease");
+                    if (settings.rotate == true) {
+                        $(this).css("transform", "rotateY(360deg)");
+                        $(this).css("-webkit-transform", "rotateY(360deg)");
+                    }
+                });
+            } else {
                 $(this).text(settings.text);
                 $(this).css("color", settings.color);
                 $(this).css("background", settings.background);
                 if (settings.bordercolor == "none") {
                     $(this).css("border", "none");
                 } else {
-                $(this).css("border", "1px solid" + settings.border);
+                    $(this).css("border", "1px solid" + settings.border);
                 }
                 $(this).css("transition", settings.transition + "ms" + " " + "ease");
-                if (settings.rotate == "yes") {
+                if (settings.rotate == true) {
                     $(this).css("transform", "rotateY(360deg)");
                     $(this).css("-webkit-transform", "rotateY(360deg)");
                 }
-            });
+            }
         });
     }
 }(jQuery));
